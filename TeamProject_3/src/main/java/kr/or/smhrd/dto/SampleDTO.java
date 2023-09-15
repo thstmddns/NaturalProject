b415932e93cfaf41dba2842978a9d2991cc320c5
thstmddns <thstmddns@naver.com>
2023-09-15 16:49:24 +0900

    no message

---
+++ TeamProject_3/src/main/java/kr/or/smhrd/dto/SampleDTO.java
@@ -0,0 +1,13 @@
+package kr.or.smhrd.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SampleDTO {
+	private String name;
+	private int age;
+}
---
+++ TeamProject_3/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,7 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<main>
+
+<P> main </P>
+
+</main>
