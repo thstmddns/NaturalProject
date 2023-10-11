fa8e7dd3a812bdaed3a47bd59cc46d3fb5a9b8fa
Sooin <tkrhkthsu503@naver.com>
2023-10-11 14:41:56 +0900

    no message

--- TeamProject_3/src/main/java/kr/or/ozz/controller/MissionController.java
+++ TeamProject_3/src/main/java/kr/or/ozz/controller/MissionController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.smart.home.dto.ReplyDTO;
+import kr.or.ozz.dto.ReplyDTO;
 
 import kr.or.ozz.dto.MissionDTO;
 import kr.or.ozz.dto.PagingDTO;
